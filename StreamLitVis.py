dd06b14dd7483a9478da4b84bc77baf4ac5b2782
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 20:37:55 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -2,9 +2,48 @@ import streamlit as st
 import pandas as pd
 from gsheetsdb import connect
 
+from utils import chart, db
+
 # Create a connection object.
 conn = connect()
 sheet_url = st.secrets["public_gsheets_url"]
+
+#THIS IS FETCHING THE DATA FROM THE GOOGLE SHEET USING SQL QUERY
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+
+
+
+
+# # THIS IS HOW I LEARNED HOW TO SHOW THE ROWS
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
+
+#THIS LITTLE SECTION WILL ACTUALLY SHOW SMTH
+for row in rows:
+    st.table(rows)
+    
+st.write("**Add your own comment:**")
+    form = st.form("comment")
+    name = form.text_input("Name")
+    comment = form.text_area("Comment")
+    submit = form.form_submit_button("Add comment")
+
+    if submit:
+#         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        db.insert(conn, [[name, comment, date]])
+#         if "just_posted" not in st.session_state:
+#             st.session_state["just_posted"] = True
+#         st.experimental_rerun()
+
+
 
 # rows1 = conn.execute(f'SELECT * FROM "{sheet_url}"')
 # df1 = pd.DataFrame(rows1)
@@ -15,32 +54,3 @@ sheet_url = st.secrets["public_gsheets_url"]
 # result = df1.loc[[option]]
 # st.write(result)
 
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# # Print results.
-# for row in rows:
-#     st.write(f"{row.name} has a :{row.pet}:")
-
-for row in rows:
-    st.table(rows)
-    
-# st.write("**Add your own comment:**")
-#     form = st.form("comment")
-#     name = form.text_input("Name")
-#     comment = form.text_area("Comment")
-#     submit = form.form_submit_button("Add comment")
-
-#     if submit:
-# #         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-#         db.insert(conn, [[name, comment, date]])
-# #         if "just_posted" not in st.session_state:
-# #             st.session_state["just_posted"] = True
-# #         st.experimental_rerun()

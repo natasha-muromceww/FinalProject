d813514480a5fc523178d108c013d1fa69d167c8
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 11:14:59 -0500

    Update TempScrap.py

--- TempScrap.py
+++ TempScrap.py
@@ -156,3 +156,18 @@
 # google-auth
 # google-auth-httplib2
 # protobuf
+
+# port = 465  # For SSL
+# smtp_server = "smtp.gmail.com"
+# sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
+# receiver_email = "cellis22@andover.edu"  # Enter receiver address
+# password = "cs630final"
+# message = """
+# Subject: Hi there
+
+# This message is sent from Python."""
+
+# context = ssl.create_default_context()
+# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, message)

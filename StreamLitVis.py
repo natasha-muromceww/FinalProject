1812e13d54cbf6299e7338bdb6ca52e7d7eb94d5
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 11:30:25 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -18,13 +18,13 @@ from shillelagh.backends.apsw.db import connect
 
 #url: https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing
 
-#Page setup 
+#Page setup-----------------------------------------------------------
 st.title("Live Poll PA")
 
 image = Image.open('DataVisForm.png')
 st.image(image, caption='Google Form QR Code')
    
-#SHILLELAGH VERSION
+#SHILLELAGH VERSION GRABBING DATA FROM SHEET -----------------------------------------------------
 connection = connect(":memory:")
 cursor = connection.cursor()
 
@@ -32,6 +32,7 @@ query = """
 SELECT * FROM "https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing"
 """
 
+#USING QUERY TO MAKE DF -----------------------------------------------------------
 data = []
 
 for row in cursor.execute(query): #row is probs tuple 
@@ -41,6 +42,34 @@ df = pd.DataFrame(data, columns=['a', 'b', 'c', 'd'])
 st.table(df)
 
 
+#SENDING COMPLIMENT-----------------------------------------------------------
+# import smtplib, ssl
+   
+def send_compliment(new_receiver_email, new_message):
+   port = 465  # For SSL
+   smtp_server = "smtp.gmail.com"
+   sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
+   receiver_email = new_receiver_email  # Enter receiver address
+   password = "cs630final"
+   message = new_message
+   context = ssl.create_default_context()
+   with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(sender_email, receiver_email, message)
+   
+send_compliment('nmuromcew22@andover.edu', 'i love you')
+
+#if new, send compliment use cache 
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+for i in range(df):
+   if date < df.loc[i, 'a'] 
+         send_compliment(df.loc[i, 'b'], df.loc[i, 'c'])
+
+                   
+#VISUALIZATIONS--------------------------------------------------------------
 #worldcloud
 
 # # my_list=["one", "one two", "three three three"]
@@ -55,41 +84,4 @@ st.table(df)
 # plt.savefig("your_file_name"+".png", bbox_inches='tight')
 # plt.show()
 # plt.close()
-
-  
-   
-#SENDING COMPLIMENT 
-# import smtplib, ssl
-
-# port = 465  # For SSL
-# smtp_server = "smtp.gmail.com"
-# sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
-# receiver_email = "cellis22@andover.edu"  # Enter receiver address
-# password = "cs630final"
-# message = """
-# Subject: Hi there
-
-# This message is sent from Python."""
-
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, message)
-   
-def send_compliment(new_receiver_email, new_message):
-   port = 465  # For SSL
-   smtp_server = "smtp.gmail.com"
-   sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
-   receiver_email = new_receiver_email  # Enter receiver address
-   password = "cs630final"
-   message = new_message
-   context = ssl.create_default_context()
-   with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-      server.login(sender_email, password)
-      server.sendmail(sender_email, receiver_email, message)
-   
-   
-   
-send_compliment('nmuromcew22@andover.edu', 'i love you')
-
 

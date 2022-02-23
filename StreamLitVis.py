f13b167944b4fb7678341cf21605552ee42422bb
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 11:07:48 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from PIL import Image
 
 #EMAIL IMPORT
-# import smtplib, ssl
+import smtplib, ssl
 
 
 # import matplotlib.pyplot as plt
@@ -61,18 +61,18 @@ st.table(df)
 #SENDING COMPLIMENT 
 # import smtplib, ssl
 
-# port = 465  # For SSL
-# smtp_server = "smtp.gmail.com"
-# sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
-# receiver_email = "cellis22@andover.edu"  # Enter receiver address
-# password = "cs630final"
-# message = """
-# Subject: Hi there
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "andovercomplimentwall@gmail.com"  # Enter your address
+receiver_email = "cellis22@andover.edu"  # Enter receiver address
+password = "cs630final"
+message = """
+Subject: Hi there
 
-# This message is sent from Python."""
+This message is sent from Python."""
 
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, message)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
 

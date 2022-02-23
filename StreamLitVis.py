bcdc9fd6527e5da4daa60eb4252aaafc608f7f0e
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 09:16:43 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -1,34 +1,45 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from gsheetsdb import connect
+# from gsheetsdb import connect
+from shillelagh.backends.apsw.db import connect
 
 
 #Page setup 
 st.title("Live Poll PA")
 
 
+# #GSHEETS VERSION
+# # Create a connection object.
+# conn = connect()
 
-# Create a connection object.
-conn = connect()
+# # Perform SQL query on the Google Sheet.
+# # Uses st.cache to only rerun when the query changes or after 10 min.
+# @st.cache(ttl=600)
+# def run_query(query):
+#     row = conn.execute(query, headers=1)
+#     return row
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    row = conn.execute(query, headers=1)
-    return row
+# sheet_url = st.secrets["public_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# # # Print results.
+# # for row in rows:
+# #     st.table(rows)
 
-# # Print results.
-# for row in rows:
-#     st.table(rows)
-
-for i in range(rows):
-    new_row = run_query(f'SELECT * FROM "{sheet_url}"')
-    st.table(new_row)
+# for i in range(rows):
+#     new_row = run_query(f'SELECT * FROM "{sheet_url}"')
+#     st.table(new_row)
    
+#SHILLELAGH VERSION
+connection = connect(":memory:")
+cursor = connection.cursor()
+
+query = """
+SELECT * FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+"""
+for row in cursor.execute(query):
+    st.write(row)
+
     
 

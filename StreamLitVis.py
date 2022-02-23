b61202dd8850bb9f64c227d6cba0e4c05d159754
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 20:39:01 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -31,14 +31,14 @@ for row in rows:
     st.table(rows)
     
 st.write("**Add your own comment:**")
-    form = st.form("comment")
-    name = form.text_input("Name")
-    comment = form.text_area("Comment")
-    submit = form.form_submit_button("Add comment")
+   form = st.form("comment")
+   name = form.text_input("Name")
+   comment = form.text_area("Comment")
+   submit = form.form_submit_button("Add comment")
 
-    if submit:
+   if submit:
 #         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
+       db.insert(conn, [[name, comment, date]])
 #         if "just_posted" not in st.session_state:
 #             st.session_state["just_posted"] = True
 #         st.experimental_rerun()

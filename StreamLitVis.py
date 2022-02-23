c304749d4f589f254d92deb62acdea2c43b819b8
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 20:02:26 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -14,6 +14,9 @@ def run_query(query):
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
+# # Print results.
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
+
 for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.table(rows)

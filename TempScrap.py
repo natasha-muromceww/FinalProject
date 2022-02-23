352d8f0e32a3dd59bf3e906e228ba173b417d1b8
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 22:31:11 -0500

    Create TempScrap.py

---
+++ TempScrap.py
@@ -0,0 +1,16 @@
+# from gsheetsdb import connect
+# import chart
+# import base
+# from google.oauth2 import service_account
+# from googleapiclient.discovery import build
+
+
+#CAN MOSTLY IGNORE BELOW THIS
+
+# # THIS IS HOW I LEARNED HOW TO SHOW THE ROWS
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
+
+#THIS LITTLE SECTION WILL ACTUALLY SHOW SMTH
+# for row in rows:
+#     st.table(rows)

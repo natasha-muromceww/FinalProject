0814c2fc5699746066c074011d838fd742462131
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 22:28:24 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 # from gsheetsdb import connect
-import chart
-import base
+# import chart
+# import base
 # from google.oauth2 import service_account
 # from googleapiclient.discovery import build
 
@@ -12,8 +12,8 @@ from shillelagh.backends.apsw.db import connect
 
 
 # Create a connection object.
-conn = connect()
-sheet_url = st.secrets["public_gsheets_url"]
+# conn = connect()
+# sheet_url = st.secrets["public_gsheets_url"]
 
 # connection = connect(":memory:")
 # cursor = connection.cursor()
@@ -25,17 +25,37 @@ sheet_url = st.secrets["public_gsheets_url"]
 # def run_query(query): 
 
 
+if __name__ == "__main__":
+    connection = connect(":memory:")
+    cursor = connection.cursor()
+
+    SQL = """
+    SELECT *
+    FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+    """
+    for row in cursor.execute(SQL):
+        print(row)
+
+# SELECT name, SUM(age)
+# FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+# WHERE age > 0
+# # GROUP BY country
+
+# UPDATE "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+# SET cnt = cnt + 1
+# WHERE name == 'Stacy'
+
 
 
 #THIS IS FETCHING THE DATA FROM THE GOOGLE SHEET USING SQL QUERY
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
+# @st.cache(ttl=600)
+# def run_query(query):
+#     rows = conn.execute(query, headers=1)
+#     return rows
 
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 #PAGE SET UP
 st.title("Andover Compliment Page")

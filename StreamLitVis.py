c3044957ad6d76136feea7f0f63d88bf46a9f5ca
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 22:39:33 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -1,40 +1,35 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-# from gsheetsdb import connect
-# import chart
-# import base
-# from google.oauth2 import service_account
-# from googleapiclient.discovery import build
-
 
 from shillelagh.backends.apsw.db import connect
 
+# google_sheet = "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
 
 # Create a connection object.
 # conn = connect()
-# sheet_url = st.secrets["public_gsheets_url"]
+sheet_url = st.secrets["public_gsheets_url"]
 
-# connection = connect(":memory:")
-# cursor = connection.cursor()
+connection = connect(":memory:")
+cursor = connection.cursor()
 
-# query = "SELECT * FROM '{sheet_url}'"
-# for row in cursor.execute(query):
-#     print(row)
+query = "SELECT * FROM '{sheet_url}'"
+for row in cursor.execute(query):
+    st.write(row)
    
 # def run_query(query): 
 
 
-if __name__ == "__main__":
-    connection = connect(":memory:")
-    cursor = connection.cursor()
+# if __name__ == "__main__":
+#     connection = connect(":memory:")
+#     cursor = connection.cursor()
 
-    SQL = """
-    SELECT *
-    FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-    """
-    for row in cursor.execute(SQL):
-        st.write(row)
+#     SQL = """
+#     SELECT *
+#     FROM sheet_url
+#     """
+#     for row in cursor.execute(SQL):
+#         st.write(row)
 
 # SELECT name, SUM(age)
 # FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
@@ -60,8 +55,6 @@ if __name__ == "__main__":
 #PAGE SET UP
 st.title("Andover Compliment Page")
 
-
-    
 st.write("**Send a Compliment:**")
 form = st.form("comment")
 name = form.text_input("Compliment Recipient")
@@ -69,15 +62,5 @@ comment = form.text_area("Comment")
 submit = form.form_submit_button("Add comment")
 
 # if submit:
-#     db.inset(conn, [[name, commen
-
-
-#CAN MOSTLY IGNORE BELOW THIS
-
-# # THIS IS HOW I LEARNED HOW TO SHOW THE ROWS
-# for row in rows:
-#     st.write(f"{row.name} has a :{row.pet}:")
-
-#THIS LITTLE SECTION WILL ACTUALLY SHOW SMTH
-# for row in rows:
-#     st.table(rows)
+#    UPDATE sheet_url
+#    SET 

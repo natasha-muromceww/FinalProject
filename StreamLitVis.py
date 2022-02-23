b78a15d1e2adeb7661cdc1460eeb8e70f475ac29
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-22 22:48:02 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -8,6 +8,7 @@ from shillelagh.backends.apsw.db import connect
 connection = connect(":memory:")
 cursor = connection.cursor()
 
+
 query = """
 SELECT * FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
 """
@@ -15,37 +16,18 @@ for row in cursor.execute(query):
     st.write(row)
    
 
-# if __name__ == "__main__":
-#     connection = connect(":memory:")
-#     cursor = connection.cursor()
-
-#     SQL = """
-#     SELECT *
-#     FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-#     """
-#     for row in cursor.execute(SQL):
-#         st.write(row)
-
-# SELECT name, SUM(age)
-# FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-# WHERE age > 0
-# # GROUP BY country
-
-# UPDATE "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-# SET cnt = cnt + 1
-# WHERE name == 'Stacy'
+UPDATE "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+SET age = age + 1
+WHERE name == 'Stacy'
 
 
+query = """
+SELECT * FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
+"""
+for row in cursor.execute(query):
+    st.write(row)
 
-#THIS IS FETCHING THE DATA FROM THE GOOGLE SHEET USING SQL QUERY
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     return rows
 
-# rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 #PAGE SET UP
 st.title("Andover Compliment Page")

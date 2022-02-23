16132227aeeed704a2db07003a643c58c231fd68
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 10:49:35 -0500

    Update TempScrap.py

--- TempScrap.py
+++ TempScrap.py
@@ -127,3 +127,25 @@
 #     df.append(row)
 
 # st.table(df)
+
+# #GSHEETS VERSION
+# # Create a connection object.
+# conn = connect()
+
+# # Perform SQL query on the Google Sheet.
+# # Uses st.cache to only rerun when the query changes or after 10 min.
+# @st.cache(ttl=600)
+# def run_query(query):
+#     row = conn.execute(query, headers=1)
+#     return row
+
+# sheet_url = st.secrets["public_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# # # Print results.
+# # for row in rows:
+# #     st.table(rows)
+
+# for i in range(rows):
+#     new_row = run_query(f'SELECT * FROM "{sheet_url}"')
+#     st.table(new_row)

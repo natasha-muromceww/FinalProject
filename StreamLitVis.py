790e468b98a34c6a231d234b8b9dad309efa6337
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 08:52:28 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -3,17 +3,19 @@ import pandas as pd
 import altair as alt
 # import time
 
-# from shillelagh.backends.apsw.db import connect
+from shillelagh.backends.apsw.db import connect
 
-# connection = connect(":memory:")
-# cursor = connection.cursor()
+# url: https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing
+
+connection = connect(":memory:")
+cursor = connection.cursor()
 
 
-# query = """
-# SELECT * FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-# """
-# for row in cursor.execute(query):
-#     st.write(row)
+query = """
+SELECT * FROM "https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing"
+"""
+for row in cursor.execute(query):
+    st.write(row)
    
 # query = """
 # UPDATE "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
@@ -29,27 +31,27 @@ import altair as alt
 # for row in cursor.execute(query):
 #     st.write(row)
 
-data = {'recipient': ['Natasha'], 'compliment': ['Hii']}
-chart = st.table(data)
-# df = pd.DataFrame(data)
-# st.write(df)
+# data = {'recipient': ['Natasha'], 'compliment': ['Hii']}
+# chart = st.table(data)
+# # df = pd.DataFrame(data)
+# # st.write(df)
 
-def add_row(recipient, compliment):
-   new_row = {'recipient': [recipient], 'compliment': [compliment]}
-   chart.add_rows(new_row)
-   return chart
+# def add_row(recipient, compliment):
+#    new_row = {'recipient': [recipient], 'compliment': [compliment]}
+#    chart.add_rows(new_row)
+#    return chart
 
 #PAGE SET UP
-st.title("Andover Compliment Page")
+# st.title("Andover Compliment Page")
 
-st.write("**Send a Compliment:**")
-form = st.form("comment")
-recipient = form.text_input("Compliment Recipient")
-compliment = form.text_area("Compliment")
-submit = form.form_submit_button("Add comment")
+# st.write("**Send a Compliment:**")
+# form = st.form("comment")
+# recipient = form.text_input("Compliment Recipient")
+# compliment = form.text_area("Compliment")
+# submit = form.form_submit_button("Add comment")
 
-if submit:
-   add_row(recipient, compliment)
+# if submit:
+#    add_row(recipient, compliment)
    
    
    

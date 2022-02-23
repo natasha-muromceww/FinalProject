13fcd9109ff4f159c3badc17be466505b625173a
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-02-23 09:08:48 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -3,29 +3,11 @@ import pandas as pd
 import altair as alt
 from gsheetsdb import connect
 
-# import time
 
-# from shillelagh.backends.apsw.db import connect
+#Page setup 
+st.title("Live Poll PA")
 
-# url: https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing
 
-# connection = connect(":memory:")
-# cursor = connection.cursor()
-
-# #Create a connection object.
-# conn = connect()
-# sheet_url = st.secrets["public_gsheets_url"]
-
-# df1 = pd.DataFrame()
-# my_table = st.table(df1)
-
-# query = """
-# SELECT * FROM "https://docs.google.com/spreadsheets/d/17bNU8T92Bu1OxNNvFMhVlkPtXw56hVSJlyXQhYxUwOw/edit?usp=sharing"
-# """
-# for row in cursor.execute(query):
-#     my_table.add_rows(row)
-
-# st.write(my_table)
 
 # Create a connection object.
 conn = connect()
@@ -44,53 +26,7 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 for row in rows:
     st.table(rows)
 
+    
    
     
-    
- 
-   
-# query = """
-# UPDATE "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-# SET age = 10
-# WHERE name == 'Chris'
-# """
 
-# # time.sleep(1)
-
-# query = """
-# SELECT * FROM "https://docs.google.com/spreadsheets/d/1v9jM22s_60OrW9O_fSHPQQa1VjV0MLJeg1rum9-UBco/edit?usp=sharing"
-# """
-# for row in cursor.execute(query):
-#     st.write(row)
-
-# data = {'recipient': ['Natasha'], 'compliment': ['Hii']}
-# chart = st.table(data)
-# # df = pd.DataFrame(data)
-# # st.write(df)
-
-# def add_row(recipient, compliment):
-#    new_row = {'recipient': [recipient], 'compliment': [compliment]}
-#    chart.add_rows(new_row)
-#    return chart
-
-#PAGE SET UP
-# st.title("Andover Compliment Page")
-
-# st.write("**Send a Compliment:**")
-# form = st.form("comment")
-# recipient = form.text_input("Compliment Recipient")
-# compliment = form.text_area("Compliment")
-# submit = form.form_submit_button("Add comment")
-
-# if submit:
-#    add_row(recipient, compliment)
-   
-   
-   
-#    data1 = {'recipient': [recipient], 'compliment': [compliment]}
-#    chart.add_rows({'recipient': [recipient], 'compliment': [compliment]})
-#    data1 = {'recipient': [recipient], 'compliment': [compliment]}
-#    chart.add_rows(data1)
-
-
-#CODE NOTES: you left off trying to append enw data to the chart

68049b2c6faceddd2529b18811eaa3ed7119d4c5
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-03-01 16:42:47 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -3,12 +3,10 @@ import pandas as pd
 import altair as alt
 
 from datetime import datetime
-# from FinalProject import DataVisForm.png
 from PIL import Image
 
 #EMAIL IMPORT
 import smtplib, ssl
-
 
 # import matplotlib.pyplot as plt
 # from wordcloud import WordCloud
@@ -77,11 +75,11 @@ def send_compliment(new_receiver_email, new_message):
 #SENDING EMAILS-----------------------------------------------------------
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-run_datetime = datetime.now()
+the_datetime = datetime.now()
 # ten_minutes = datetime(0, 0, 0, 0, 10, 0, 0)
 # cache_time = run_datetime - ten_minutes
 
-st.write(datetime_object)
+st.write(the_datetime)
 # st.write(ten_minutes)
 # st.write(cache_time)
 

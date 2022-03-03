cee7287a6a9b2f9bc8e80f5ee2c7a013d2cc73bd
natasha-muromceww <95489542+natasha-muromceww@users.noreply.github.com>
2022-03-02 19:51:16 -0500

    Update StreamLitVis.py

--- StreamLitVis.py
+++ StreamLitVis.py
@@ -81,11 +81,11 @@ def send_compliment(new_receiver_email, new_message):
 # # Uses st.cache to only rerun when the query changes or after 10 min.
 # @st.cache(ttl=600)
 the_datetime = datetime.now()
-# ten_minutes = datetime(0, 0, 0, 0, 10, 0, 0)
+ten_minutes = datetime(0, 0, 0, 0, 10, 0, 0)
 # cache_time = run_datetime - ten_minutes
 
 st.write(the_datetime)
-# st.write(ten_minutes)
+st.write(ten_minutes)
 # st.write(cache_time)
 
 
